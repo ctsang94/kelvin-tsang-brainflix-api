192711d6354d00bafeac5918dbe5a71ae1c7e262
Kelvin <ctsang94@gmail.com>
2024-09-24 00:18:43 -0400

    add index.html public page

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use('/videos', videosRoute)
 app.use(express.static('public'))
 
 const PORT = process.env.PORT || 8080
+
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
 })
